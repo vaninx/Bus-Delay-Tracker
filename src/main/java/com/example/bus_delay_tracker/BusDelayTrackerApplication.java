4e9dd1244695307bf260847aef5aa2b7c69140b2
vaninx <vaninwabuoku@gmail.com>
2025-05-13 22:33:12 +0100

    start

---
+++ src/main/java/com/example/bus_delay_tracker/BusDelayTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.bus_delay_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusDelayTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusDelayTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bus_delay_tracker/BusDelayTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bus_delay_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusDelayTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
